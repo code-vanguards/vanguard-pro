429d9678ddd50dd83f9065f461aee9187d1822e0
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-02-28 09:25:24 -0500

    Completed initial working version of NewProject component.

--- src/components/NewProject.vue
+++ src/components/NewProject.vue
@@ -1,10 +1,65 @@
 <template>
-  <span>Placeholder</span>
+  <form @submit.prevent="$emit('add-project', projectName)">
+    <label class="formItem">Add project</label>
+    <input class="formItem" type="text" v-model="projectName"/>
+    <button class="formItem">Add</button>
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  emits: ['add-project'],
+  data() {
+    return {
+      projectName: '',
+    };
+  },
+};
 </script>
 
 <style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #0ed145;
+  padding: 5px 10px 10px 10px;
+  border-radius: 10px;
+  width: 85%;
+  margin: auto;
+  text-align: center;
+}
+
+form > .formItem {
+  margin-top: 5px;
+}
+
+form label {
+  color: #f0f0f0;
+}
+
+form input {
+  border-radius: 5px;
+  border-style: none;
+  padding: 5px;
+}
+
+form button {
+  border-radius: 3px;
+  width: 4rem;
+  padding: 3px;
+  color: white;
+  font-weight: bold;
+  box-shadow: 0 0 3px #000000;
+  border-style: none;
+  background-color: #ff7f27;
+}
+
+form button:hover {
+  background-color: #ffa465;
+}
+
+form button:active {
+  background-color: #ff7f27;
+}
 </style>
