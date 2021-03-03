50b03c75504f9f7b57752007a10dbc5f67030a3b
Foster Hangdaan <foster.hangdaan@gmail.com>
2021-03-02 21:34:21 -0500

    Removed console logs.

--- src/components/StreakCounter.vue
+++ src/components/StreakCounter.vue
@@ -18,29 +18,23 @@ export default {
   },
   methods: {
     checkStreak() {
-      console.log('In addStreak()');
       const now = new Date();
       const oneDay = 86400000;
 
       if (this.streak.length > 0) {
         let delta = now.getTime() - this.streak[0].getTime();
         if (delta > oneDay) {
-          console.log('You lost your streak!');
           this.streak = [new Date()];
         } else {
-          console.log('You keep your streak!');
           this.streak.unshift(now);
         }
       } else {
-        console.log('Array is empty. Adding streak...');
         this.streak.unshift(now);
       }
     },
   },
   watch: {
     tasksToday(value) {
-      console.log('In StreakCounter, tasksToday() watcher');
-      console.log(value);
       const dailyGoal = 1;
       const goalMet = dailyGoal === value;
       if (goalMet) this.checkStreak();
